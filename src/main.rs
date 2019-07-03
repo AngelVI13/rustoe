b65ad1203f63c1c08890f19f6e7eaaa92c1a7a17
Angel ILIEV <a.v.iliev13@gmail.com>
2019-07-03 09:22:10 +0300

    Added ability to compute result lines dynamically and added example for columns

--- src/main.rs
+++ src/main.rs
@@ -23,6 +23,7 @@ struct Board {
     // fixed size array (instead of Vec) and a counter to keep
     // track of the move number will be more performant
     history: Vec<usize>,
+    result_lines: (Vec<Vec<usize>>, Vec<Vec<usize>>, Vec<Vec<usize>>),
 }
 
 impl fmt::Display for Board {
@@ -39,6 +40,7 @@ impl Board {
             pos: [Mark::NoPlayer; BOARD_SIZE],
             player_just_moved: Mark::O,
             history: Vec::new(),
+            result_lines: get_result_lines(),
         }
     }
 
@@ -99,6 +101,20 @@ impl Board {
     }
 
     fn get_result(&self, player_jm: Mark) -> Option<f32> {
+        let (col_vec, row_vec, diag_vec) = &self.result_lines;
+
+        for col in col_vec.iter() {
+            // let mut result: Vec<Mark> = Vec::new();
+            let result = col.iter().map(|x| self.pos[*x] as i8).collect::<Vec<i8>>();
+            // the first element of result vec would also be the winner mark if line has result
+            let potential_winner = result[0];
+            // sum all elements in a row to find if there is a winner
+            let result: i8 = result.iter().sum();
+            if result.abs() == ROWS as i8 {
+                return if potential_winner == player_jm as i8 { Some(WIN) } else { Some(LOSS) };
+            }
+        }
+
         for idx in 0..ROWS {
             // Checks result column-wise i.e. 0-3-6/1-4-7/2-5-8 indexes
             if self.pos[idx] == self.pos[idx + ROWS] && self.pos[idx] == self.pos[idx + 2*ROWS] && self.pos[idx] != Mark::NoPlayer {
@@ -163,20 +179,60 @@ fn main() {
     println!("{:?}", b);
     b.take_move();
     println!("{:?}", b);
+}
 
+fn get_row_array(col_arr: &Vec<Vec<usize>>) -> Vec<Vec<usize>> {
+    let mut row_arr: Vec<Vec<usize>> = vec![vec![0; ROWS]; ROWS];
+
+    for i in 0..ROWS {
+        for j in 0..ROWS {
+            // performs the equivalent of zip on a list of lists in python
+            // [[1, 2], [3, 4]] -> [[1, 3], [2, 4]]
+            row_arr[i][j] = col_arr[j][i];
+        }
+    }
+
+    row_arr
+}
+
+fn get_column_array() -> Vec<Vec<usize>> {
     let mut col_arr: Vec<Vec<usize>> = vec![vec![0; ROWS]; ROWS];
 
-    get_column_array(&mut col_arr);
+    // take every nth element from board starting from each of the first row columns
+    // i.e. 0-3-6, 1-4-7 etc.
+    for i in 0..ROWS {
+        let v_temp = (i..BOARD_SIZE).
+            step_by(ROWS).
+            collect::<Vec<usize>>();
 
-    // how to do zip(col_array) to go from 0-3-6|1-4-7|2-5-8 -> 0-1-2|3-4-5|6-7-8
-    // for arbitrary sub vectors into multi dimensional vector
-    println!("{:?}", col_arr);
+        col_arr[i] = v_temp;
+    }
+
+    col_arr
 }
 
 
-fn get_column_array(v: &mut Vec<Vec<usize>>) {
+fn get_diagonals(row_vec: &Vec<Vec<usize>>) -> Vec<Vec<usize>> {
+    let mut left_diag: Vec<usize> = vec![0; ROWS];
+    let mut right_diag: Vec<usize> = vec![0; ROWS];
+    let mut diagonals: Vec<Vec<usize>> = Vec::new();
+
     for i in 0..ROWS {
-        let v_temp = (i..BOARD_SIZE).step_by(ROWS).collect::<Vec<usize>>();
-        v[i] = v_temp;
+        left_diag[i] = row_vec[i][i]; // left diagonal -> 0,0| 1,1| 2,2
+        right_diag[i] = row_vec[i][ROWS-i-1] // right diagonal 0,2 | 1,1| 2,0 .
+        // Additional -1 is needed to convert from size to index
     }
+    diagonals.push(left_diag);
+    diagonals.push(right_diag);
+
+    diagonals
+}
+
+
+fn get_result_lines() -> (Vec<Vec<usize>>, Vec<Vec<usize>>, Vec<Vec<usize>>) {
+    let col_vec = get_column_array();
+    let row_vec = get_row_array(&col_vec);
+    let diag_vec = get_diagonals(&row_vec);
+
+    (col_vec, row_vec, diag_vec)
 }
