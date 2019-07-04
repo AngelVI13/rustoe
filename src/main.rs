2191173e88a8ac3974a5c0c2c2d9483a9aecd64f
Angel ILIEV <a.v.iliev13@gmail.com>
2019-07-04 08:10:08 +0300

    Replaced get_result logic with evaluating result lines for winner

--- src/main.rs
+++ src/main.rs
@@ -23,7 +23,7 @@ struct Board {
     // fixed size array (instead of Vec) and a counter to keep
     // track of the move number will be more performant
     history: Vec<usize>,
-    result_lines: (Vec<Vec<usize>>, Vec<Vec<usize>>, Vec<Vec<usize>>),
+    result_lines: Vec<Vec<Vec<usize>>>,
 }
 
 impl fmt::Display for Board {
@@ -100,43 +100,27 @@ impl Board {
         return possible_moves;
     }
 
-    fn get_result(&self, player_jm: Mark) -> Option<f32> {
-        let (col_vec, row_vec, diag_vec) = &self.result_lines;
-
-        for col in col_vec.iter() {
+    fn evaluate_lines(&self, lines: &Vec<Vec<usize>>, player_jm: &Mark) -> Option<f32> {
+        for line in lines.iter() {
             // let mut result: Vec<Mark> = Vec::new();
-            let result = col.iter().map(|x| self.pos[*x] as i8).collect::<Vec<i8>>();
+            let result = line.iter().map(|x| self.pos[*x] as i8).collect::<Vec<i8>>();
             // the first element of result vec would also be the winner mark if line has result
             let potential_winner = result[0];
             // sum all elements in a row to find if there is a winner
             let result: i8 = result.iter().sum();
             if result.abs() == ROWS as i8 {
-                return if potential_winner == player_jm as i8 { Some(WIN) } else { Some(LOSS) };
+                return if potential_winner == *player_jm as i8 { Some(WIN) } else { Some(LOSS) };
             }
         }
 
-        for idx in 0..ROWS {
-            // Checks result column-wise i.e. 0-3-6/1-4-7/2-5-8 indexes
-            if self.pos[idx] == self.pos[idx + ROWS] && self.pos[idx] == self.pos[idx + 2*ROWS] && self.pos[idx] != Mark::NoPlayer {
-                return if self.pos[idx] == player_jm { Some(WIN) } else { Some(LOSS) };
+        return None;
+    }
+
+    fn get_result(&self, player_jm: Mark) -> Option<f32> {
+        for line in self.result_lines.iter() {
+            if let Some(winner) = self.evaluate_lines(&line, &player_jm) {
+                return Some(winner);
             }
-
-            let idx = idx * ROWS;
-
-            // Checks result row-wise i.e. 0-1-2/3-4-5/6-7-8
-            if self.pos[idx] == self.pos[idx + 1] && self.pos[idx] == self.pos[idx + 2] && self.pos[idx] != Mark::NoPlayer {
-                return if self.pos[idx] == player_jm { Some(WIN) } else { Some(LOSS) };
-            }
-        }
-
-        // Check result for left diagonal i.e. 0-4-8
-        if self.pos[0] == self.pos[ROWS + 1] && self.pos[0] == self.pos[2*ROWS + 2] && self.pos[0] != Mark::NoPlayer {
-            return if self.pos[0] == player_jm { Some(WIN) } else { Some(LOSS) };
-        }
-
-        // Check result for left diagonal i.e. 2-4-6
-        if self.pos[ROWS - 1] == self.pos[2*ROWS - 2] && self.pos[ROWS - 1] == self.pos[2*ROWS] && self.pos[ROWS - 1] != Mark::NoPlayer {
-            return if self.pos[ROWS - 1] == player_jm { Some(WIN) } else { Some(LOSS) };
         }
 
         // If no result and no moves left => DRAW
@@ -160,43 +144,43 @@ fn main() {
     println!("{}", b);
     b.take_move();
     b.make_move(0);
-    // b.make_move(4);
-    // b.make_move(8);
-    // b.make_move(1);
-    // b.make_move(7);
-    // b.make_move(6);
-    // b.make_move(2);
+    b.make_move(4);
+    b.make_move(8);
+    b.make_move(1);
+    b.make_move(7);
+    b.make_move(2);
+    b.make_move(6);
     // b.make_move(5);
     // b.make_move(3);
 
-    let _result = b.make_move_safe(4).unwrap();
+    // let _result = b.make_move_safe(4).unwrap();
 
+    println!("{:?}", b);
     let result = b.get_result(Mark::X);
     println!("{:?}", result);
     let moves = b.get_moves();
     println!("{:?}", moves);
 
-    println!("{:?}", b);
     b.take_move();
     println!("{:?}", b);
 }
 
-fn get_row_array(col_arr: &Vec<Vec<usize>>) -> Vec<Vec<usize>> {
-    let mut row_arr: Vec<Vec<usize>> = vec![vec![0; ROWS]; ROWS];
+fn get_row_vector(col_vec: &Vec<Vec<usize>>) -> Vec<Vec<usize>> {
+    let mut row_vec: Vec<Vec<usize>> = vec![vec![0; ROWS]; ROWS];
 
     for i in 0..ROWS {
         for j in 0..ROWS {
             // performs the equivalent of zip on a list of lists in python
             // [[1, 2], [3, 4]] -> [[1, 3], [2, 4]]
-            row_arr[i][j] = col_arr[j][i];
+            row_vec[i][j] = col_vec[j][i];
         }
     }
 
-    row_arr
+    row_vec
 }
 
-fn get_column_array() -> Vec<Vec<usize>> {
-    let mut col_arr: Vec<Vec<usize>> = vec![vec![0; ROWS]; ROWS];
+fn get_column_vector() -> Vec<Vec<usize>> {
+    let mut col_vec: Vec<Vec<usize>> = vec![vec![0; ROWS]; ROWS];
 
     // take every nth element from board starting from each of the first row columns
     // i.e. 0-3-6, 1-4-7 etc.
@@ -205,34 +189,33 @@ fn get_column_array() -> Vec<Vec<usize>> {
             step_by(ROWS).
             collect::<Vec<usize>>();
 
-        col_arr[i] = v_temp;
+        col_vec[i] = v_temp;
     }
 
-    col_arr
+    col_vec
 }
 
 
 fn get_diagonals(row_vec: &Vec<Vec<usize>>) -> Vec<Vec<usize>> {
-    let mut left_diag: Vec<usize> = vec![0; ROWS];
-    let mut right_diag: Vec<usize> = vec![0; ROWS];
-    let mut diagonals: Vec<Vec<usize>> = Vec::new();
+    let mut left_diagonal: Vec<usize> = vec![0; ROWS];
+    let mut right_diagonal: Vec<usize> = vec![0; ROWS];
 
     for i in 0..ROWS {
-        left_diag[i] = row_vec[i][i]; // left diagonal -> 0,0| 1,1| 2,2
-        right_diag[i] = row_vec[i][ROWS-i-1] // right diagonal 0,2 | 1,1| 2,0 .
+        left_diagonal[i] = row_vec[i][i]; // left diagonal -> 0,0| 1,1| 2,2
+        right_diagonal[i] = row_vec[i][ROWS-i-1] // right diagonal 0,2 | 1,1| 2,0 .
         // Additional -1 is needed to convert from size to index
     }
-    diagonals.push(left_diag);
-    diagonals.push(right_diag);
 
+    let diagonals = vec![left_diagonal, right_diagonal];
     diagonals
 }
 
 
-fn get_result_lines() -> (Vec<Vec<usize>>, Vec<Vec<usize>>, Vec<Vec<usize>>) {
-    let col_vec = get_column_array();
-    let row_vec = get_row_array(&col_vec);
-    let diag_vec = get_diagonals(&row_vec);
+fn get_result_lines() -> Vec<Vec<Vec<usize>>> {
+    let col_vec = get_column_vector();
+    let row_vec = get_row_vector(&col_vec);
+    let diagonal_vec = get_diagonals(&row_vec);
 
-    (col_vec, row_vec, diag_vec)
+    let result_lines = vec![col_vec, row_vec, diagonal_vec];
+    result_lines
 }
